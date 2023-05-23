931f2823ae484c9e05392cc539c1198cd22ec6a1
ElisMalkova <132060477+ElisMalkova@users.noreply.github.com>
2023-05-23 12:32:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber > secondNumber)
+// {
+//     Console.WriteLine("Число " + firstNumber + " максимальное число " );
+//     Console.WriteLine("Число " + secondNumber + " минимальное число " );
+// }
+// else 
+// {
+//     Console.WriteLine("Число " + secondNumber + " максимальное число " );
+//     Console.WriteLine("Число " + firstNumber + " минимальное число " );
+// }
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число: ");
+// int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (firstNumber > secondNumber) 
+//    if (firstNumber > thirdNumber)
+//    {
+//        Console.WriteLine("Число " + firstNumber + " максимальное число ");
+//    }
+//    else
+//    {
+//     if (firstNumber < secondNumber)
+//        if (secondNumber > thirdNumber)
+//        {
+//         Console.WriteLine("Число " + secondNumber + " максимальное число ");
+//        }
+//    }
+//    else
+//    {
+//         Console.WriteLine("Число " + thirdNumber + " максимальное число ");
+//    }    
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+// Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// int remDiv = firstNumber % 2;
+
+// if (remDiv == 0)
+// {
+//     Console.WriteLine("Число " + firstNumber + " -> да ");
+// }
+// else
+// {
+//     Console.WriteLine("Число " + firstNumber + " -> нет ");
+// }
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+// Console.Write("Введите число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// int evenNumber = 2;
+// if(firstNumber > 1)
+// {
+//     while(evenNumber <= firstNumber)
+//     {
+//         Console.Write(evenNumber + " ");
+//         evenNumber = evenNumber + 2;
+//     }
+// }
